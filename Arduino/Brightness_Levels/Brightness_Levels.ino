385d0f6a9113c5ec7dd0751d96b13790ab89c16b
petervuong <peter.vuong@mail.utoronto.ca>
2017-07-02 16:52:04 -0400

    Arduino

---
+++ Arduino/Brightness_Levels/Brightness_Levels.ino
@@ -0,0 +1,67 @@
+int button = 8;
+int LED = 5; 
+int LEDLevel = 0; 
+boolean lastButton = LOW;
+boolean currentButton = LOW;
+int LEDLevelCopy = 255;
+
+
+
+boolean debounce(boolean last)
+
+{
+  boolean current = digitalRead(button);
+  if (last != current)
+  {
+    delay(5);
+    current = digitalRead(button);
+  }
+  return current;
+  
+}
+
+
+
+void setup() {
+
+pinMode(button,INPUT);
+pinMode(LED,OUTPUT);
+
+}
+
+void loop() 
+
+
+{
+
+currentButton = debounce(lastButton);
+
+if(currentButton == HIGH && lastButton == LOW && LEDLevel != 255)
+
+{
+  LEDLevel = LEDLevel + 51; 
+analogWrite(LED, LEDLevel);
+
+}
+
+else if (currentButton == HIGH && lastButton == LOW && LEDLevel == 255 && LEDLevelCopy != 0) 
+
+{
+  
+     LEDLevelCopy = LEDLevelCopy - 51;
+  analogWrite(LED, LEDLevelCopy);
+
+}
+
+else if (currentButton == HIGH && lastButton == LOW && LEDLevelCopy == 0)
+{
+ 
+  analogWrite(LED, LEDLevelCopy);
+   LEDLevel = 0;
+  LEDLevelCopy = 255; 
+}
+
+lastButton = currentButton; 
+
+
+}
---
+++ Arduino/Button_switch/Button_switch.ino
@@ -0,0 +1,58 @@
+
+
+int button = 8; 
+int LED = 13;
+boolean lastButton = LOW;
+boolean LEDStatus = false;
+boolean currentButton = LOW;
+
+// Fixes light source from bouncing
+
+boolean debounce(boolean last)
+
+{
+  boolean current = digitalRead(button);
+  if (last != current)
+  {
+    delay(5);
+    current = digitalRead(button);
+  }
+  return current;
+  
+}
+
+
+
+
+void setup() {
+
+
+// Set IO pins 
+
+pinMode(button, INPUT);
+pinMode(LED, OUTPUT);
+
+
+ 
+
+}
+
+void loop() {
+
+// You want the status of the current button to update after debouncing
+
+currentButton = debounce(lastButton);
+
+if(currentButton == HIGH && lastButton == LOW)
+
+{
+
+  LEDStatus =!LEDStatus;
+ 
+}
+
+
+  lastButton = currentButton;
+  digitalWrite(LED,LEDStatus);
+
+}
